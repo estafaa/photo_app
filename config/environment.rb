c40100f1ef9a4926ee56b74752461abb5c2fe205
Anton <faa@phystech.edu>
2017-11-03 20:26:42 +0000

    changes on grid..

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,8 @@ ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['estafaa'],
-    :password => ENV['flameniksus'],
+    :user_name => ENV['app80192549@heroku.com'],
+    :password => ENV['R2x-8mL-fCd-8n2'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
 }
